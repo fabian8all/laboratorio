fb87a5d53e7757de629546936c44dfffb6b5d576
Ramón Díaz <jramondiaz96@gmail.com>
2020-11-06 18:04:42 -0600

    modal agregar estudio

--- vistas/cotizacion.php
+++ vistas/cotizacion.php
@@ -27,7 +27,7 @@
                     <option value="2">TODOS</option>
                   </select>
                   <div class="input-group-append">
-                    <button class="btn btn-info" type="button">
+                    <button class="btn btn-info" type="button" data-toggle="modal" data-target="#modAgregarCliente">
                       <i class="fas fa-plus fa-sm"></i>
                     </button>
                   </div>
@@ -68,7 +68,7 @@
       </a>
       <!-- Card Content - Collapse -->
       <div class="collapse show" id="TotalCard">
-        <div class="card-body" >
+        <div class="card-body">
           <div class="row">
             <div class="col-lg-8 col-md-8 col-12">
             </div>
@@ -155,57 +155,32 @@
       </div>
       <div class="modal-body">
         <form id="formAgregar" class="user">
-          <div class="form-group">
-            <input type="text" class="form-control form-control-user necesary" id="txtNombres" placeholder="Nombres*">
-          </div>
-          <div class="form-group row">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="form-group">
+                <input type="text" class="form-control form-control-user necesary" id="txtNombres" placeholder="Nombres*">
+              </div>
+            </div>
             <div class="col-lg-6 col-md-6 col-12">
-              <input type="text" class="form-control form-control-user necesary" id="txtPrimerAp" placeholder="Primer apellido*">
+              <div class="form-group">
+                <input type="text" class="form-control form-control-user necesary" name="" value="" placeholder="Genero*">
+              </div>
             </div>
-            <div class="form-group col-lg-6 col-md-6 col-12 smallMargin">
-              <input type="text" class="form-control form-control-user necesary" id="txtSegundoAp" placeholder="Segundo apellido*">
-            </div>
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control form-control-user " id="txtCalle" placeholder="Calle">
-          </div>
-          <div class="form-group row">
-            <div class="col-lg-4 col-md-4 col-12">
-              <input type="text" class="form-control form-control-user " id="txtNoExt" placeholder="No. Exterior">
-            </div>
-            <div class="form-group col-lg-4 col-md-4 col-12 smallMargin">
-              <input type="text" class="form-control form-control-user" id="txtNoInt" placeholder="No. Interior">
-            </div>
-            <div class="form-group col-lg-4 col-md-4 col-12 ">
-              <input type="text" class="form-control form-control-user " id="txtColonia" placeholder="Colonia">
-            </div>
-          </div>
-          <div class="form-group row">
             <div class="col-lg-6 col-md-6 col-12">
-              <input type="text" class="form-control form-control-user" id="txtEntreCalle1" placeholder="Entre calle">
+              <div class="form-group">
+                <input type="number" class="form-control form-control-user necesary" name="" value="" placeholder="Edad*">
+              </div>
             </div>
-            <div class="col-lg-6 col-md-6 col-12 smallMargin">
-              <input type="text" class="form-control form-control-user" id="txtEntreCalle2" placeholder="Entre calle">
+            <div class="col-lg-6 col-md-6 col-12">
+              <div class="form-group">
+                <input type="tel" class="form-control form-control-user necesary" name="" value="" placeholder="Teléfono*">
+              </div>
             </div>
-          </div>
-          <div class="form-group row">
-            <div class="col-lg-4 col-md-4 col-12">
-              <input type="text" class="form-control form-control-user " id="txtEstado" placeholder="Estado">
+            <div class="col-lg-6 col-md-6 col-12">
+              <div class="form-group">
+                <input type="email" class="form-control form-control-user necesary" name="" value="" placeholder="Correo electrónico*">
+              </div>
             </div>
-            <div class="col-lg-4 col-md-4 col-12 smallMargin">
-              <input type="text" class="form-control form-control-user " id="txtMunicipio" placeholder="Municipio">
-            </div>
-            <div class="col-lg-4 col-md-4 col-12 smallMargin">
-              <input type="text" class="form-control form-control-user necesary" id="txtTelefono" placeholder="Teléfono*">
-            </div>
-          </div>
-          <div class="form-group row">
-            <div class="col-lg-12 col-md-12 col-12">
-              <input type="text" class="form-control form-control-user" id="txtCorreoElec" placeholder="Correo electrónico">
-            </div>
-            <!--<div class="col-lg-6 col-md-6 col-12">
-                <input type="date" class="form-control form-control-user" id="txtFechaRegistro*" placeholder="Fecha de registro">
-              </div>-->
           </div>
       </div>
       <div class="modal-footer">
@@ -217,8 +192,8 @@
   </div>
 </div>
 <!-- MODAL EDITAR -->
-<div class="modal fade" id="modEditar" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+<div class="modal fade" id="modAgregarEstudio" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" style="max-width: 900px;" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLongTitle">Editar cliente</h5>
@@ -227,78 +202,128 @@
         </button>
       </div>
       <div class="modal-body">
-        <form id="formAgregarE" class="user">
-          <div class="form-group" style="display: none">
-            <input type="text" class="form-control form-control-user necesary" id="txtIdE" placeholder="">
+        <div class="row">
+          <div class="col-lg-8 col-md-8 col-12">
           </div>
-          <div class="form-group" style="display: none">
-            <input type="text" class="form-control form-control-user necesary" id="txtIdUE" placeholder="">
+          <div class="col-lg-4 col-md-4 col-12 smallMargin">
+            <form>
+              <div class="input-group">
+                <input type="text" id="txt_busqueda" class="form-control bg-light border-0 small" style="background-color: white !important" placeholder="Buscar cliente..." aria-label="Search" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                  <button class="btn btn-primary" type="button">
+                    <i class="fas fa-search fa-sm"></i>
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-          <div class="form-group text-center">
-            <label><b>Nombres*</b></label>
-            <input type="text" class="form-control form-control-user necesary" id="txtNombresE" placeholder="Nombres*">
+        </div>
+        <div style="margin-top: 20px" id="carouselExampleControls" class="carousel " data-ride="">
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="row">
+                <div class="col-lg-4 col-md-4 col-12">
+                  <div class="card">
+                    <div class="card-header">
+                      <span style="display:inline-block" class="btn btn-info">4568</span>
+                      <h6 style="display:inline-block">VDRL EM SUERO</h6>
+                    </div>
+                    <div class="card-body">
+                      <div class="form-group">
+                        <h6>SUERO 1 ML AYUNO 8HRS. REFRIGERAR</h6>
+                      </div>
+                      <div class="form-group">
+                        <p style="display:inline-block">Tiempo:</p>
+                        <label style="display:inline-block">1</label>
+                      </div>
+                      <div class="form-group">
+                        <p style="display:inline-block">Costo:</p>
+                        <label style="display:inline-block">$100</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                  <div class="card">
+                    <div class="card-header">
+                      <span style="display:inline-block" class="btn btn-info">4568</span>
+                      <h6 style="display:inline-block">VDRL EM SUERO</h6>
+                    </div>
+                    <div class="card-body">
+                      <div class="form-group">
+                        <h6>SUERO 1 ML AYUNO 8HRS. REFRIGERAR</h6>
+                      </div>
+                      <div class="form-group">
+                        <p style="display:inline-block">Tiempo:</p>
+                        <label style="display:inline-block">1</label>
+                      </div>
+                      <div class="form-group">
+                        <p style="display:inline-block">Costo:</p>
+                        <label style="display:inline-block">$100</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                  <div class="card">
+                    <div class="card-header">
+                      <span style="display:inline-block" class="btn btn-info">4568</span>
+                      <h6 style="display:inline-block">VDRL EM SUERO</h6>
+                    </div>
+                    <div class="card-body">
+                      <div class="form-group">
+                        <h6>SUERO 1 ML AYUNO 8HRS. REFRIGERAR</h6>
+                      </div>
+                      <div class="form-group">
+                        <p style="display:inline-block">Tiempo:</p>
+                        <label style="display:inline-block">1</label>
+                      </div>
+                      <div class="form-group">
+                        <p style="display:inline-block">Costo:</p>
+                        <label style="display:inline-block">$100</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-lg-4 col-md-4 col-12">
+                  <div class="card">
+                    <div class="card-header">
+                      <span style="display:inline-block" class="btn btn-info">4568</span>
+                      <h6 style="display:inline-block">VDRL EM SUERO</h6>
+                    </div>
+                    <div class="card-body">
+                      <div class="form-group">
+                        <h6>SUERO 1 ML AYUNO 8HRS. REFRIGERAR</h6>
+                      </div>
+                      <div class="form-group">
+                        <p style="display:inline-block">Tiempo:</p>
+                        <label style="display:inline-block">1</label>
+                      </div>
+                      <div class="form-group">
+                        <p style="display:inline-block">Costo:</p>
+                        <label style="display:inline-block">$100</label>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="..." alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="..." alt="Third slide">
+            </div>
           </div>
-          <div class="form-group row">
-            <div class="col-lg-6 col-md-6 col-12 text-center">
-              <label><b>Primer apellido*</b></label>
-              <input type="text" class="form-control form-control-user necesary" id="txtPrimerApE" placeholder="Primer apellido*">
-            </div>
-            <div class="col-lg-6 col-md-6 col-12 text-center">
-              <label><b>Segundo apellido*</b></label>
-              <input type="text" class="form-control form-control-user necesary" id="txtSegundoApE" placeholder="Segundo apellido*">
-            </div>
-          </div>
-          <div class="form-group text-center">
-            <label><b>Calle</b></label>
-            <input type="text" class="form-control form-control-user " id="txtCalleE" placeholder="Calle">
-          </div>
-          <div class="form-group row">
-            <div class="col-lg-4 col-md-4 col-12 text-center">
-              <label><b>No. Exterior</b></label>
-              <input type="text" class="form-control form-control-user " id="txtNoExtE" placeholder="No. Exterior">
-            </div>
-            <div class="col-lg-4 col-md-4 col-12 text-center">
-              <label><b>No. Interior</b></label>
-              <input type="text" class="form-control form-control-user" id="txtNoIntE" placeholder="No. Interior">
-            </div>
-            <div class="col-lg-4 col-md-4 col-12 text-center">
-              <label><b>Colonia</b></label>
-              <input type="text" class="form-control form-control-user " id="txtColoniaE" placeholder="Colonia">
-            </div>
-          </div>
-          <div class="form-group row">
-            <div class="col-lg-6 col-md-6 col-12 text-center">
-              <label><b>Entre calle</b></label>
-              <input type="text" class="form-control form-control-user" id="txtEntreCalle1E" placeholder="Entre calle">
-            </div>
-            <div class="col-lg-6 col-md-6 col-12 text-center">
-              <label><b>Entre calle</b></label>
-              <input type="text" class="form-control form-control-user" id="txtEntreCalle2E" placeholder="Entre calle">
-            </div>
-          </div>
-          <div class="form-group row">
-            <div class="col-lg-4 col-md-4 col-12 text-center">
-              <label><b>Estado</b></label>
-              <input type="text" class="form-control form-control-user " id="txtEstadoE" placeholder="Estado">
-            </div>
-            <div class="col-lg-4 col-md-4 col-12 text-center">
-              <label><b>Municipio</b></label>
-              <input type="text" class="form-control form-control-user " id="txtMunicipioE" placeholder="Municipio">
-            </div>
-            <div class="col-lg-4 col-md-4 col-12 text-center">
-              <label><b>Teléfono*</b></label>
-              <input type="text" class="form-control form-control-user necesary" id="txtTelefonoE" placeholder="Teléfono*">
-            </div>
-          </div>
-          <div class="form-group row">
-            <div class="col-lg-12 col-md-12 col-12 text-center">
-              <label><b>Correo electrónico</b></label>
-              <input type="text" class="form-control form-control-user" id="txtCorreoElecE" placeholder="Correo electrónico">
-            </div>
-            <!--<div class="col-lg-6 col-md-6 col-12">
-                <input type="date" class="form-control form-control-user " id="txtFechaRegistroE" placeholder="Fecha de registro">
-              </div>-->
-          </div>
+          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
