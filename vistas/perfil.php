0b94e289fc264ca9c75408c488a5f534158add5b
Ramón Díaz <jramondiaz96@gmail.com>
2020-11-18 13:34:37 -0600

    vista perfiles

---
+++ vistas/perfil.php
@@ -0,0 +1,287 @@
+<h1 class="h3 mb-2 text-gray-800">Perfiles</h1>
+<div class="row">
+  <div class="col-lg-6 col-md-6 col-12">
+    <!-- Collapsable Card Example -->
+    <div class="card shadow mb-4">
+      <!-- Card Header - Accordion -->
+      <a href="#cotizacionCard" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="cotizacionCard">
+        <h6 class="m-0 font-weight-bold text-primary"><i class="fas fa-clipboard-list"></i> Cotización</h6>
+      </a>
+      <!-- Card Content - Collapse -->
+      <div class="collapse" id="cotizacionCard">
+        <div class="card-body">
+          <div id="tableContainer" class="table-responsive">
+            <table class="table table-bordered table-sm" id="dataTable" width="100%" cellspacing="0">
+              <thead id="thead" class="text-center">
+                <th>PERFIL</th>
+                <th>VER</th>
+                <th>SOLICITAR ESTUDIOS</th>
+              </thead>
+              <tbody id="tbody" class="text-center">
+                <tr>
+                	<td>ADMINISTRADOR</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+                <tr>
+                	<td>PACIENTE</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-6 col-md-6 col-12">
+    <!-- Collapsable Card Example -->
+    <div class="card shadow mb-4">
+      <!-- Card Header - Accordion -->
+      <a href="#resultadosCard" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="resultadosCard">
+        <h6 class="m-0 font-weight-bold text-primary"><i class="fas fa-clipboard-list"></i> Resultados</h6>
+      </a>
+      <!-- Card Content - Collapse -->
+      <div class="collapse" id="resultadosCard">
+        <div class="card-body">
+        	<div id="tableContainer" class="table-responsive">
+            <table class="table table-bordered table-sm" id="dataTable" width="100%" cellspacing="0">
+              <thead id="thead" class="text-center">
+                <th>PERFIL</th>
+                <th>VER</th>
+                <th>TOMAR MUESTRA</th>
+                <th>SUBIR RESULTADOS</th>
+                <th>DESCARGAR RESULTADOS</th>
+                <th>GUARDAR PAGO</th>
+              </thead>
+              <tbody id="tbody" class="text-center">
+                <tr>
+                	<td>ADMINISTRADOR</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+                <tr>
+                	<td>PACIENTE</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-6 col-md-6 col-12">
+    <!-- Collapsable Card Example -->
+    <div class="card shadow mb-4">
+      <!-- Card Header - Accordion -->
+      <a href="#estudiosCard" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="estudiosCard">
+        <h6 class="m-0 font-weight-bold text-primary"><i class="fas fa-clipboard-list"></i> Estudios</h6>
+      </a>
+      <!-- Card Content - Collapse -->
+      <div class="collapse" id="estudiosCard">
+        <div class="card-body">
+        	<div id="tableContainer" class="table-responsive">
+            <table class="table table-bordered table-sm" id="dataTable" width="100%" cellspacing="0">
+              <thead id="thead" class="text-center">
+                <th>PERFIL</th>
+                <th>VER</th>
+                <th>CREAR</th>
+                <th>ACTUALIZAR</th>
+                <th>BORRAR</th>
+              </thead>
+              <tbody id="tbody" class="text-center">
+                <tr>
+                	<td>ADMINISTRADOR</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+                <tr>
+                	<td>PACIENTE</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-6 col-md-6 col-12">
+    <!-- Collapsable Card Example -->
+    <div class="card shadow mb-4">
+      <!-- Card Header - Accordion -->
+      <a href="#pacienteCard" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="pacienteCard">
+        <h6 class="m-0 font-weight-bold text-primary"><i class="fas fa-clipboard-list"></i> Pacientes</h6>
+      </a>
+      <!-- Card Content - Collapse -->
+      <div class="collapse" id="pacienteCard">
+        <div class="card-body">
+        	<div id="tableContainer" class="table-responsive">
+            <table class="table table-bordered table-sm" id="dataTable" width="100%" cellspacing="0">
+              <thead id="thead" class="text-center">
+                <th>PERFIL</th>
+                <th>VER</th>
+                <th>CREAR</th>
+                <th>ACTUALIZAR</th>
+                <th>BORRAR</th>
+              </thead>
+              <tbody id="tbody" class="text-center">
+                <tr>
+                	<td>ADMINISTRADOR</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+                <tr>
+                	<td>PACIENTE</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-6 col-md-6 col-12">
+    <!-- Collapsable Card Example -->
+    <div class="card shadow mb-4">
+      <!-- Card Header - Accordion -->
+      <a href="#clientesCard" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="clientesCard">
+        <h6 class="m-0 font-weight-bold text-primary"><i class="fas fa-clipboard-list"></i> Clientes</h6>
+      </a>
+      <!-- Card Content - Collapse -->
+      <div class="collapse" id="clientesCard">
+        <div class="card-body">
+        	<div id="tableContainer" class="table-responsive">
+            <table class="table table-bordered table-sm" id="dataTable" width="100%" cellspacing="0">
+              <thead id="thead" class="text-center">
+                <th>PERFIL</th>
+                <th>VER</th>
+                <th>CREAR</th>
+                <th>ACTUALIZAR</th>
+                <th>BORRAR</th>
+              </thead>
+              <tbody id="tbody" class="text-center">
+                <tr>
+                	<td>ADMINISTRADOR</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+                <tr>
+                	<td>PACIENTE</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-6 col-md-6 col-12">
+    <!-- Collapsable Card Example -->
+    <div class="card shadow mb-4">
+      <!-- Card Header - Accordion -->
+      <a href="#UsuariosCard" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="UsuariosCard">
+        <h6 class="m-0 font-weight-bold text-primary"><i class="fas fa-clipboard-list"></i> Usuarios</h6>
+      </a>
+      <!-- Card Content - Collapse -->
+      <div class="collapse" id="UsuariosCard">
+        <div class="card-body">
+        	<div id="tableContainer" class="table-responsive">
+            <table class="table table-bordered table-sm" id="dataTable" width="100%" cellspacing="0">
+              <thead id="thead" class="text-center">
+                <th>PERFIL</th>
+                <th>VER</th>
+                <th>CREAR</th>
+                <th>ACTUALIZAR</th>
+                <th>BORRAR</th>
+              </thead>
+              <tbody id="tbody" class="text-center">
+                <tr>
+                	<td>ADMINISTRADOR</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+                <tr>
+                	<td>PACIENTE</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-6 col-md-6 col-12">
+    <!-- Collapsable Card Example -->
+    <div class="card shadow mb-4">
+      <!-- Card Header - Accordion -->
+      <a href="#PerfilesCard" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="PerfilesCard">
+        <h6 class="m-0 font-weight-bold text-primary"><i class="fas fa-clipboard-list"></i> Perfiles</h6>
+      </a>
+      <!-- Card Content - Collapse -->
+      <div class="collapse" id="PerfilesCard">
+        <div class="card-body">
+        	<div id="tableContainer" class="table-responsive">
+            <table class="table table-bordered table-sm" id="dataTable" width="100%" cellspacing="0">
+              <thead id="thead" class="text-center">
+                <th>PERFIL</th>
+                <th>VER</th>
+                <th>CREAR</th>
+                <th>ACTUALIZAR</th>
+                <th>BORRAR</th>
+              </thead>
+              <tbody id="tbody" class="text-center">
+                <tr>
+                	<td>ADMINISTRADOR</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+                <tr>
+                	<td>PACIENTE</td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                  <td><input type="checkbox" name="" value=""></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
